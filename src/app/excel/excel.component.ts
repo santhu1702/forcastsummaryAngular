5e89bf017d7605d40df910af56b8603739c32cca
santhu1702 <58586232+santhu1702@users.noreply.github.com>
2023-04-12 14:18:45 +0530

    binding summary data using query querySelector

--- src/app/excel/excel.component.ts
+++ src/app/excel/excel.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ExcelService } from '../_Services/excel.service';
 import { subCategory } from 'src/app/_models/subCategory';
 import { SummaryData } from '../_models/SummaryData';
+import { HotTableComponent } from '@handsontable/angular';
+import Handsontable from 'handsontable';
+import HyperFormula from 'hyperformula';
 
 @Component({
   selector: 'app-excel',
@@ -9,23 +12,75 @@ import { SummaryData } from '../_models/SummaryData';
   styleUrls: ['./excel.component.css'],
 })
 export class ExcelComponent {
-  constructor(private ExcelService: ExcelService) {}
-  dataset: any = this.getsummarydata();
+  data1: any;
+  hotElement!: HotTableComponent;
+
+  constructor(private excelService: ExcelService) { }
+
+  // data1 = [
+  //   ['10.26', null, 'Sum', '=SUM(A:A)'],
+  //   ['20.12', null, 'Average', '=AVERAGE(A:A)'],
+  //   ['30.01', null, 'Median', '=MEDIAN(A:A)'],
+  //   ['40.29', null, 'MAX', '=MAX(A:A)'],
+  //   ['50.18', null, 'MIN', '=MIN(A1:A5)'],
+  //   [''],
+  //   ['=SUM(A1:A5)', null, '', '']
+  // ];
 
   ngOnInit(): void {
-    this.getsubCategory();
-    this.getsummarydata();
+    this.binddata();
+
   }
 
+  hyperformulaInstance = HyperFormula.buildEmpty({
+    licenseKey: 'internal-use-in-handsontable',
+  });
+
   getsubCategory() {
-    this.ExcelService.getsubCategory().subscribe((category) => {
+    this.excelService.getsubCategory().subscribe((category) => {
       return category;
     });
   }
-  getsummarydata() {
-    this.ExcelService.SummaryData().subscribe((response) => {
-      this.dataset = response;
-      return response;
+
+
+
+  async getsummarydata() {
+    return new Promise((resolve, reject) => {
+      this.excelService.SummaryData().subscribe({
+        next: (response) => {
+          const summaryData = response.map(item => Object.values(item).slice(1));
+          resolve(summaryData);
+        },
+        error: (error) => {
+          reject(error);
+        }
+      });
     });
   }
- }
+  
+  
+
+  async binddata() {
+    const container1 = document.querySelector('#example-basic-multi-sheet-1');
+    debugger
+    await this.getsummarydata().then(
+      (res) => {
+        this.data1 = res
+      }
+      
+    ); 
+    console.log(this.data1)
+    if (container1) {
+      new Handsontable(container1, {
+        data:  this.data1,
+        colHeaders: true,
+        rowHeaders: true,
+        height: 'auto',
+        formulas: {
+          engine: this.hyperformulaInstance,
+        },
+        licenseKey: 'non-commercial-and-evaluation'
+      });
+    }
+  }
+}
