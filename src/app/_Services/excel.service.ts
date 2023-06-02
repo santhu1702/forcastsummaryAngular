926aa9a673f486e9a3e71c974cd5da7fb20f184b
santhu1702 <58586232+santhu1702@users.noreply.github.com>
2023-06-02 12:57:25 +0530

    label changes

--- src/app/_Services/excel.service.ts
+++ src/app/_Services/excel.service.ts
@@ -24,21 +24,34 @@ export class ExcelService {
     return this.http.get<DropDownsData>(this.baseurl + 'DropDownsData');
   }
 
-   
+
   SummaryDataByFilters(summaryData: SummaryDataByFilters) {
     console.log("summaryData", summaryData)
     const url = `${this.baseurl}SummaryDataByFilters`;
-    const body = JSON.stringify(summaryData); 
+    const body = JSON.stringify(summaryData);
     return this.http.post<SummaryDataByBrand>(url, body, {
       headers: new HttpHeaders({ "Content-Type": "application/json" })
-    }); 
+    });
   }
 
+
+  
   uploadData(file: any) {
     const url = `${this.baseurl}extractFileUpload`;
     const formData = new FormData();
-    formData.append('file', file); 
+    formData.append('file', file);
     return this.http.post<any>(url, formData);
   }
-  
+
+  getTemplateData() {
+    return this.http.get<any>(this.baseurl + 'getData');
+  }
+
+  uploadExcelData(data: any) {
+     const url = `${this.baseurl}upload`;
+    const body = JSON.stringify(data);
+    return this.http.post<SummaryDataByBrand>(url, body, {
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    });
+  }
 }
--- src/app/excel/excel.component.ts
+++ src/app/excel/excel.component.ts
@@ -21,13 +21,14 @@ export class ExcelComponent {
   ddnBrands: any = [];
   ddnYears: any = [];
   ddnrollUps: string[] = ['Quarter MAT', 'Half MAT', 'Full Year'];
-  ddnMeasure: any[] = [ '$ Sales CATEGORY', '% Chg CATEGORY', '$ Sales UNILEVER', '% Chg UNILEVER', 'UL $ Share' ];
+  ddnMeasure: any[] = ['$ Sales CATEGORY', '% Chg CATEGORY', '$ Sales UNILEVER', '% Chg UNILEVER', 'UL $ Share'];
+  hotInstance!: Handsontable;
 
   dropDownForm = this._FormBuilder.group({
     Brands: new FormControl({ value: ['all'], disabled: false }),
     subCategory: new FormControl({ value: ['all'], disabled: false }),
     DataSource: new FormControl({ value: ['all'], disabled: false }),
-    Years: new FormControl({ value: ['all'], disabled: false }, [ Validators.required ]),
+    Years: new FormControl({ value: ['all'], disabled: false }, [Validators.required]),
     rollup: new FormControl({ value: ['all'], disabled: false }),
     Measure: new FormControl({ value: 'all', disabled: false }, [Validators.required]),
   });
@@ -36,13 +37,15 @@ export class ExcelComponent {
     private excelService: ExcelService,
     private _FormBuilder: FormBuilder,
     private loader: SpinnerService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.loader.showSpinner();
     this.getDropdowndata().then(() => {
       this.loader.hideSpinner();
     });
+    Handsontable.renderers.registerRenderer('negativeValueRenderer', this.negativeValueRenderer);
+
   }
 
   hyperformulaInstance = HyperFormula.buildEmpty({
@@ -92,21 +95,34 @@ export class ExcelComponent {
           reject(error);
         },
       });
-    }); 
+    });
+  }
+
+  async getTemplateData() {
+    return new Promise((resolve, reject) => {
+      this.excelService.getTemplateData().subscribe({
+        next: (response) => {
+          console.log('getTemplateData', response);
+          resolve(response);
+        },
+        error: (error) => {
+          reject(error);
+        },
+      });
+    });
   }
 
   async bindData(data: any) {
     const container1 = document.querySelector('#example-basic-multi-sheet-1');
     try {
       // const res = await this.getSummaryData();
-      let res : any  = await this.getSummaryDataByBrand(data);
-      console.log("BindData_Data",res)
+      let res: any = await this.getTemplateData();
+      console.log("BindData_Data", res[0])
       this.excelData = res;
-      let hotInstance: Handsontable;
       if (container1) {
         container1.innerHTML = '';
-        hotInstance = new Handsontable(container1, {
-          data: res.data,
+        this.hotInstance = new Handsontable(container1, {
+          data: res,
           colHeaders: true,
           rowHeaders: true,
           width: '100%',
@@ -114,8 +130,50 @@ export class ExcelComponent {
           formulas: {
             engine: this.hyperformulaInstance,
           },
-          mergeCells:res.mergeData,
+          // mergeCells:res.mergeData,
           licenseKey: 'non-commercial-and-evaluation',
+          hiddenColumns: {
+            columns: [16],
+            indicators: true,
+          },
+
+          mergeCells: [
+            { row: 1, col: 1, rowspan: 3, colspan: 1 },
+            { row: 5, col: 1, rowspan: 3, colspan: 1 },
+            { row: 9, col: 1, rowspan: 3, colspan: 1 },
+            { row: 13, col: 1, rowspan: 3, colspan: 1 },
+            { row: 17, col: 1, rowspan: 9, colspan: 1 },
+            { row: 27, col: 1, rowspan: 3, colspan: 1 },
+            { row: 31, col: 1, rowspan: 3, colspan: 1 },
+            { row: 35, col: 1, rowspan: 3, colspan: 1 },
+            { row: 39, col: 1, rowspan: 3, colspan: 1 },
+            { row: 43, col: 1, rowspan: 9, colspan: 1 },
+            { row: 53, col: 1, rowspan: 3, colspan: 1 },
+            { row: 57, col: 1, rowspan: 3, colspan: 1 },
+            { row: 61, col: 1, rowspan: 3, colspan: 1 },
+            { row: 65, col: 1, rowspan: 3, colspan: 1 },
+            { row: 69, col: 1, rowspan: 9, colspan: 1 },
+            { row: 0, col: 0, rowspan: 4, colspan: 1 },
+            { row: 4, col: 0, rowspan: 4, colspan: 1 },
+            { row: 8, col: 0, rowspan: 4, colspan: 1 },
+            { row: 12, col: 0, rowspan: 4, colspan: 1 },
+            { row: 16, col: 0, rowspan: 10, colspan: 1 },
+            { row: 26, col: 0, rowspan: 4, colspan: 1 },
+            { row: 30, col: 0, rowspan: 4, colspan: 1 },
+            { row: 34, col: 0, rowspan: 4, colspan: 1 },
+            { row: 38, col: 0, rowspan: 4, colspan: 1 },
+            { row: 42, col: 0, rowspan: 10, colspan: 1 },
+          ],
+          cells: (row: any, col: any, prop: any, ...args: any[]) => { // Use rest parameters syntax
+            const cellProperties: any = {};
+            if ([0, 4, 8, 12, 16, 26, 30, 34,38,42,52,56,60,64,68,78,82,86,90,94,98].includes(row)) {
+              cellProperties.renderer = this.firstRowRenderer.bind(this, ...args); // Bind the renderer function with arguments
+            }
+            else {
+              cellProperties.renderer = 'negativeValueRenderer'; // uses lookup map 
+            }
+            return cellProperties;
+          }
           // customBorders: [
           //   {
           //     range: { from: { row: 0, col: 0 }, to: { row: 3, col: 15 } },
@@ -138,16 +196,34 @@ export class ExcelComponent {
           //     right: { width: 1, color: '#333333' },
           //     bottom: { width: 1, color: '#333333' }
           //   }
-          // ],
-          // hiddenRows: {
-          //   rows : [2,3,4]
-          // }
+          // ] 
+
         });
       }
     } catch (error) {
       console.error('error:', error);
     }
+  };
+  firstRowRenderer = (instance: any, td: any, row: any, col: any, prop: any, value: any, cellProperties: any) => {
+    Handsontable.renderers.TextRenderer.apply(this, [instance, td, row, col, prop, value, cellProperties]); // Use apply with an array of arguments
+    td.style.fontWeight = 'bold';
+    td.style.color = 'white';
+    td.style.background = '#305496';
   }
+  negativeValueRenderer = (instance: any, td: any, row: number, col: number, prop: string | number, value: any, cellProperties: any) => {
+    Handsontable.renderers.TextRenderer.apply(this, [instance, td, row, col, prop, value, cellProperties]);
+    const columnHeader = instance.getDataAtCell(row, 2); // Assuming the column header is in the 3rd row (index 2)
+    if (col > 3 && ["UL BPS Chg", "$ % Chg","Proj $ % Chg","Proj BPS Chg"].includes(columnHeader) && !isNaN(parseFloat(value))) {
+      console.log(parseFloat(value));
+      const isNegative = parseFloat(value) < 0; 
+      console.log('isNegative',isNegative)
+      td.className = isNegative ? 'make-me-red' : 'make-me-green';
+      td.style.background = isNegative ? '#ffc7ce' : '#c6efce';
+      td.style.fontWeight = 'bold';
+    }
+      
+  };
+
 
   async getDropdowndata() {
     return new Promise((resolve, reject) => {
@@ -161,6 +237,26 @@ export class ExcelComponent {
           resolve(response);
         },
         error: (error) => {
+          reject(error);
+        },
+      });
+    });
+  }
+
+  uploadData() {
+    let data = this.hotInstance.getData();
+    console.log('UploadData', data)
+    console.log('UploadData1', this.hotInstance.getColHeader())
+    return new Promise((resolve, reject) => {
+      this.excelService.uploadExcelData(data).subscribe({
+        next: (response) => {
+          console.log('getTemplateData', response);
+          alert(response)
+          resolve(response);
+        },
+        error: (error) => {
+          console.log("uploadData_error", error)
+          alert("Something Went Wrong")
           reject(error);
         },
       });
